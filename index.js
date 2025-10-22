3424713f4be21c9138c8d3b865fb0520a26363e8
SALAHEDDIN47 <salaheddinejalloul538@gmail.com>
2025-10-22 22:12:25 +0100

    final update of the project

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const userSchema = new Schema({
     type: Number,
     default: 0
   },
-  logs:[{
+  log:[{
     description:{
       type: String
     },
@@ -57,16 +57,16 @@ app.get('/api/users', async (req, res)=>{
   res.json(allUsers);
 });
 
-const addUserLog = async(id, desc, dur, dt) => {
+const addUserLog = async(id,log) => {
   try {
     const UpdatedUser = await User.findByIdAndUpdate(
       id,
       {
         $push:{
-          logs:{
-            description: desc,
-            duration: dur,
-            date: dt || new Date()
+          log:{
+            description: log.description,
+            duration: log.duration,
+            date: log.date || new Date()
           }
         },
         $inc:{ count : 1}
@@ -75,19 +75,67 @@ const addUserLog = async(id, desc, dur, dt) => {
     );
     return UpdatedUser;
   } catch(error){
-      console.error("Error adding logs", error);
+      console.error("Error adding log", error);
       throw error;
   }
 }
 
 app.post('/api/users/:_id/exercises',async (req,res) => {
-  const {description, duration, date} = req.body;
+  const exercise = (({ description, duration, date }) => ({ 
+    description, 
+    duration: parseInt(duration), // Convert to number
+    date: date ? new Date(date) : new Date()
+  }))(req.body);
   const id = req.params._id;
-  const updatedUser = await addUserLog(id,description,duration,date);
-  console.log(updatedUser)
-  res.json(updatedUser);
+  const updatedUser = await addUserLog(id, exercise);
+  res.json({
+    username: updatedUser.username,
+    description: exercise.description,
+    duration: exercise.duration,
+    date: exercise.date.toDateString(),
+    _id: updatedUser._id
+  });
 });
 
+app.get('/api/users/:_id/logs', async(req,res)=>{
+  try{
+    const id = req.params._id;
+    let {from, to, limit} = req.query;
+    const user = await User.findById(id);
+     let logs = user.log;
+    if(from){
+      from = new Date(from);
+      if(isNaN(from.getTime())){
+       return res.status(500).json({error: "invalid from format"});
+      } logs = logs.filter(log => new Date(log.date) >= from);
+    }
+    if(to){
+      to = new Date(to);
+      if(isNaN(to.getTime())){
+       return res.status(500).json({error: "invalid to format"});
+      } logs = logs.filter(log => new Date(log.date) <= to);
+    }
+    if(limit){
+      limit = parseInt(limit);
+      if(isNaN(limit) || limit < 0){
+        return res.status(500).json({error: "invalid limit value"});
+      } logs = logs.slice(0,limit);
+    }
+    logs = logs.map(log => ({
+      description: log.description,
+      duration: log.duration,
+      date: new Date(log.date).toDateString()
+    }));
+    res.json({
+      username: user.username,
+      count: logs.length,
+      _id: user._id,
+      log: logs
+    });
+  }catch(error){
+    res.status(500).json({error: "Server error"});
+  }
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
